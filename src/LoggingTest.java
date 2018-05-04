9d120e9d127fa34c4dfb2b663274f955d53c596a
Gene Johannsen <gejohann@cisco.com>
2018-05-03 17:25:40 -0700

    More cases.  Messy, should clean up.

--- src/LoggingTest.java
+++ src/LoggingTest.java
@@ -5,59 +5,81 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggingTest {
-  private static final Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(LoggingTest.class);
 
   public static void main(final String[] args) {
-
+    /** Just an object to use as a logging example. */
     UltimateAnswer answer = new LoggingTest.UltimateAnswer();
 
-    // invalid: this logging method has 2 placeholders, but given parameter is only
-    // 1.
-    logger.info("msg: {}, {}.", "Hello");
+    /*
+     * invalid: this logging method has 2 placeholders, but only one parameter.
+     */
+    LOGGER.info("msg: {}, {}.", "Hello");
 
     // valid
-    logger.info("msg: {}, {}.", "Hello", "World");
+    LOGGER.info("msg: {}, {}.", "Hello", "World");
 
     // invalid: Throwable instance does not need placeholder
-    logger.error("msg: {}, {}", "Hello", new RuntimeException());
+    LOGGER.error("msg: {}, {}", "Hello", new RuntimeException());
 
     // valid
-    logger.error("msg: {}", "Hello", new RuntimeException());
+    LOGGER.error("msg: {}", "Hello", new RuntimeException());
 
     /*
      * Don't do this. It creates a StringBuilder to concatenate the Strings.
      */
-    logger.info("Info Logging String Concatenation: " + answer);
+    LOGGER.info("Info Logging String Concatenation: " + answer);
 
     /*
      * Don't do this. Won't get logged (because it's a trace message), but
      * toString() will still be called.
      */
-    logger.trace("Trace Logging toString(): {}", answer.toString());
+    LOGGER.trace("Trace Logging toString(): {}", answer.toString());
 
     /*
      * Do this. Won't call toString() unless logging is at trace level.
      */
-    logger.trace("Trace Logging Parameter: {}", answer);
+    LOGGER.trace("Trace Logging Parameter: {}", answer);
 
     /*
      * Do this. This will call toString() when the message is logged.
      */
-    logger.info("Info Logging Parameter: {}", answer);
-    logger.info("Info Logging Parameter: {} {}", "Something", new RuntimeException("Here in exception."));
+    LOGGER.info("Info Logging Parameter: {}", answer);
+
+    RuntimeException e = new RuntimeException("Here in exception.");
+    LOGGER.info("Info Logging Parameter Exception: ", e);
     /*
      * If you can't override toString() and need to output a log message, here is
      * one way to do it without calling the String creation method unnecessarily.
      * There are similar methods for all log levels.
      */
-    if (logger.isTraceEnabled()) {
-      logger.trace("Trace Logging Parameter and custom String: {}", answer.toTraceLoggingString());
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.trace("Trace Logging Parameter and custom String: {}", answer.toTraceLoggingString());
+    }
+
+    int a = 12;
+    int b = 222;
+    /*
+     * This boxes the calculation.
+     */
+    LOGGER.info("Here is a calc : {}", a - b);
+
+    LOGGER.info("Here is an array : {} {} {}", (Object[]) args);
+  }
+
+  public static void logIfDebugEnabled(Object... argument) {
+    if (LOGGER.isDebugEnabled()) {
+      StringBuffer logString = new StringBuffer();
+      for (Object object : argument) {
+        logString.append(object);
+      }
+      LOGGER.debug(logString.toString());
     }
   }
 
   private static class UltimateAnswer {
 
-    private final static Map<String, String> map = new HashMap<>();
+    private static final Map<String, String> map = new HashMap<>();
 
     static {
       map.put("41", "Almost");
