a1cd60232ac511d63d2bba717b980395ede9453c
Gene Johannsen <gejohann@cisco.com>
2018-05-18 16:25:46 -0700

    Documenting cases.

--- src/LoggingTest.java
+++ src/LoggingTest.java
@@ -21,7 +21,7 @@ public class LoggingTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(LoggingTest.class);
 
   /**
-   * Do something.
+   * Try to cause every kind of logging problem.
    * 
    * @param args
    *          Unused
@@ -29,50 +29,69 @@ public class LoggingTest {
   public static void main(final String[] args) {
 
     /*
-     * Create a big map of nothing but random Strings. This is something that should
-     * never be logged unless necessary for debugging.
+     * First, some general best practices for using {} place-holders when logging
+     * messages and Exceptions.
      */
-    RandomString gen = new RandomString(8, ThreadLocalRandom.current());
-    Map<String, String> m = new HashMap<>();
-    for (int i = 0; i < 1000; i++) {
-      m.put(gen.nextString(), gen.nextString());
-    }
 
-    /** An object to use as a logging example. */
-    //UltimateAnswer answer = new LoggingTest.UltimateAnswer();
-
-    // Invalid: this logging method has 2 placeholders, but only one parameter.
-    LOGGER.info("msg: {}, {}.", "Hello", m);
-
-    // Valid
+    // Valid. Two place-holders and two parameters.
     LOGGER.info("msg: {}, {}.", "Hello", "World");
 
-    // invalid: Throwable instance does not need placeholder
-    LOGGER.error("msg: {}, {}", "Hello", new RuntimeException("Wrong way to log an exception."));
+    // Invalid. This logging call has 2 place-holders, but only one parameter.
+    LOGGER.info("msg: {}, {}.", "Hello");
 
-    // valid. The Exception, including the message, will be printed in the log.
+    // Valid. How to have {} display in a logging message.
+    LOGGER.info("msg: \\{}, {}.", "World");
+
+    /*
+     * Logging Exceptions.
+     */
+    RuntimeException ex = new RuntimeException("Something is wrong!");
+
+    // Invalid. Throwable instance does not need placeholder if it is the last
+    // argument. In this case, the second place-holder is ignored.
+    LOGGER.error("msg: {}, {}", "Hello", ex);
+
+    // Valid. The Exception, including its message, will be printed in the log.
+    // There is no need to separately log the message.
     LOGGER.error("msg: {}", "Hello", new RuntimeException("Correct way to log an exception."));
 
     /*
-     * Don't do this. It creates a StringBuilder to concatenate the Strings.
+     * Create a big ugly map of nothing but random Strings. This is something that
+     * would never be logged unless necessary for debugging and takes work to
+     * generate the String representation. An important goal is to make sure logging
+     * this Map does not impact application performance.
      */
-    LOGGER.info("Info Logging String Concatenation: " + m);
+    RandomString gen = new RandomString(10, ThreadLocalRandom.current());
+    Map<String, String> bigUglyMap = new HashMap<>(1000);
+    for (int i = 0; i < 1000; i++) {
+      bigUglyMap.put(gen.nextString(), gen.nextString());
+    }
+
+    /** An object to use as a logging example. */
+    // UltimateAnswer answer = new LoggingTest.UltimateAnswer();
+
+    /*
+     * Don't do this. It creates a StringBuilder behind the scenes to concatenate
+     * the Strings. In Java, all arguments to a method have to be resolved to a
+     * primitive or Object reference before the method is called.
+     */
+    LOGGER.trace("Trace Logging String Concatenation: " + bigUglyMap);
 
     /*
      * Don't do this. Won't get logged (because it's a trace message), but
      * toString() will still be called.
      */
-    LOGGER.trace("Trace Logging toString(): {}", m.toString());
+    LOGGER.trace("Trace Logging toString(): {}", bigUglyMap.toString());
 
     /*
      * Do this. Won't call toString() unless logging is at trace level.
      */
-    LOGGER.trace("Trace Logging Parameter: {}", m);
+    LOGGER.trace("Trace Logging Parameter: {}", bigUglyMap);
 
     /*
      * Do this. This will call toString() when the message is logged.
      */
-    LOGGER.info("Info Logging Parameter: {}", m);
+    LOGGER.info("Info Logging Parameter: {}", bigUglyMap);
 
     RuntimeException e = new RuntimeException("Here in exception.");
     LOGGER.info("Info Logging Parameter Exception: ", e);
@@ -83,7 +102,8 @@ public class LoggingTest {
      * There are similar methods for all log levels.
      */
     if (LOGGER.isDebugEnabled()) {
-      LOGGER.trace("Trace Logging Parameter and custom String: {}", m.toTraceLoggingString());
+      // LOGGER.trace("Trace Logging Parameter and custom String: {}",
+      // m.toTraceLoggingString());
     }
 
     int a = 12;
